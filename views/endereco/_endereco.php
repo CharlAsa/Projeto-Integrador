6e3aea2e8160735cb2ebb45a5ae329bc4ef74802
Giliarde Macedo Alves <giliarde.macedo@academico.ifrn.edu.br>
2019-06-21 18:28:26 -0300

    Cadastro do Usuario - Sem opcao delete

---
+++ views/endereco/_endereco.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Endereco */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="endereco-form">
+
+
+    <?= $form->field($model, 'logradouro')->textInput(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'bairro')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cidade')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'uf')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cep')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'numero_casa')->textInput(['maxlength' => true]) ?>
+
+</div>
