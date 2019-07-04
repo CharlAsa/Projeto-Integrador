3fe794c5645d23f8e2d8e3227dcd3986e7565390
CharlAsa <charles_sd@hotmail.com>
2019-07-04 00:10:45 -0300

    Migração usuário padrão

--- migrations/m190508_033036_test.php
+++ migrations/m190508_033036_test.php
@@ -12,19 +12,26 @@ class m190508_033036_test extends Migration
      */
     public function safeUp()
     {
-		$auth = Yii::$app->authManager;
-		
-		$admin = $auth->createRole('admin');
-		
-		$auth->add($admin);
-		
-		$createConsulta = $auth->createPermission('consulta/create');
-        $createConsulta->description = 'Criar consulta';
-        $auth->add($createConsulta);
-		
-		$auth->addChild($admin, $createConsulta);
-		
-		$auth->assign($admin, 1);
+		$this->insert('usuario', [
+            'cpf' => '12323',
+            'rg' => '12323',
+            'username' => 'alfa',
+            'password' => Yii::$app->getSecurity()->generatePasswordHash('alfa'),
+        ]);
+        $this->insert('endereco', [
+            'id_usuario' => 1,
+            'logradouro' => '1',
+            'bairro' => '1',
+            'cidade' => '1',
+            'uf' => '1',
+            'cep' => '1',
+            'numero_casa' => '1',
+        ]);
+        $this->insert('contato', [
+            'id_usuario' => 1,
+            'numero_telefone' => '12323',
+        ]);
+        
     }
 
     /**
@@ -32,9 +39,12 @@ class m190508_033036_test extends Migration
      */
     public function safeDown()
     {
-		$auth = Yii::$app->authManager;
+        $this->delete('endereco', ['id_usuario' => 1]);
+        $this->delete('contato', ['id_usuario' => 1]);
+        $this->delete('usuario', ['username' => 'alfa']);
+		//$auth = Yii::$app->authManager;
 
-        $auth->removeAll();
+        //$auth->removeAll();
 		
         //echo "m190508_033036_test cannot be reverted.\n";
 
