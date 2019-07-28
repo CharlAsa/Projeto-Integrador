4c421113120ac7a866e26cf9796f7dd04c9e2bcc
CharlAsa <charles_sd@hotmail.com>
2019-07-28 00:56:29 -0300

    Consertar bug do usuario/update

--- views/usuario/update.php
+++ views/usuario/update.php
@@ -20,7 +20,7 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
         'model' => $model,
         'arrayContato' => $arrayContato,
         'arrayEndereco' => $arrayEndereco,
-        'model2' => $model2,
+        //'model2' => $model2,
     ]) ?>
 
 </div>
