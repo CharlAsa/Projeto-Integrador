dcaf6a8b28431b360a34c22f7d839aa39d0db067
CharlAsa <charles_sd@hotmail.com>
2019-08-03 01:18:49 -0300

    apagar coisa desnecessária

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,7 @@ $bundle = yiister\gentelella\assets\Asset::register($this);
             <div class="left_col scroll-view">
 
                 <div class="navbar nav_title" style="border: 0;">
-                    <a href="/" class="site_title"><i class="fa fa-paw"></i> <span>Edite me!</span></a>
+                    <a href="/" class="site_title"><i class="fa fa-paw"></i> <span></span></a>
                 </div>
                 <div class="clearfix"></div>
 
